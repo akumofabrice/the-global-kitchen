80a10bf94396efdfc31e7bce879bd53d6a2ca9a2
fabj <akumofabrice@gmail.com>
2026-05-18 13:44:43 +0100

    defining routes in server js

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ import express from "express";
 const app = express();
 
 app.get("/api/recipes", (reg, res) =>{
-    res.send("you got 10 recipes");
+    res.status(200).send("you got 10 recipes");
 });
+
+app.post("/api/recipes", (reg, res) =>{
+    res.status(201).json({message:"Recipes created successfully"});
+});
+
+app.put("/api/recipes:id", (reg, res) =>{
+    res.status().json({message:" recipes updated successfully"});
+});
+
+app.put("/api/recipes:id", (reg, res) =>{
+    res.status(200).json({message:" recipes deleted successfully"});
+});
+
 
 app.listen(5001, () => {
     console.log("server started on PORP: 5001");
