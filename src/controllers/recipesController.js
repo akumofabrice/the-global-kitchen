32e9c2500ef98eea5d2ca9b9f1f7202658146bff
fabj <akumofabrice@gmail.com>
2026-05-18 20:52:00 +0100

    completing controllers and middleware

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,16 +1,59 @@
-export function  getAllRecipes(reg, res) {
-    res.status(200).send("you just fetched the recipes");
+import Recipe from "../../models/Recipe.js";
+
+export async function  getAllRecipes(reg, res) {
+  try {
+       const recipes = await Recipe.find()
+       res.status(200).json(recipes)
+
+  } catch (error) {
+    console.error("Error in getAllRecipes controller",error);
+    res.status(500).json({message: "internal server error"});
+  }
 }
 
 
-export function createRecipe(reg, res) {
-    res.status(201).json({message:"Recipes created successfully"});
+export async function createRecipe(reg, res) {
+  try {
+        const {title,ingredients,instructions,cookingTime,difficulty,category} = reg.body
+        const newRecipe = new Recipe({title,ingredients,instructions,cookingTime,difficulty,category})
+
+        await newRecipe.save()
+        res.status(201).json({message:"Recipe created successfully"})
+
+  } catch (error) {
+    console.error("Error in createRecipes controller",error);
+    res.status(500).json({message: "internal server error"});
+
+  }
+    
   }
 
-export function  updateRecipe(reg, res) {
-    res.status(200).json({message:" recipes updated successfully"});
+export async function  updateRecipe(req, res) {
+  try {
+    const {title,ingredients,instructions,cookingTime,difficulty,category} =req.body
+    const updatedRecipe = await Recipe.findByIdAndUpdate(req.params.id,{title,ingredients,instructions,cookingTime,difficulty,category} ,{new:true})
+    if(!updateRecipe) return res.status(404).json({message:"Recipe not found"})
+    res.status(200).json({mesage: "Recipe updated successfully!"}); 
+
+  } catch(error) {
+    console.error("Error in updateRecipe contraller",error);
+    res.status(500).json({message:"internal server error"})
+
   }
 
-export function  deleteRecipe(reg, res) {
-    res.status(200).json({message:" recipes deleted successfully"});
   }
+
+export async function  deleteRecipe(req, res) {
+  try {
+    const {title,ingredients,instructions,cookingTime,difficulty,category} =req.body
+    const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id,{title,ingredients,instructions,cookingTime,difficulty,category} )
+    res.status(200).json({mesage: "Recipe deleted successfully!"}); 
+
+  } catch (error) {
+    console.error("Error in deleteRecipe controller",error);
+    res.status(500).json({message:"internal server error"})
+
+
+  }
+
+}
--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
  
 connectDB()
+// middleware
+app.use(express.json());
 
 app.use("/api/recipes", recipesRoutes);
 
